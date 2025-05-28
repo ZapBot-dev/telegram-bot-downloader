20a9ba7a2bfeae8ede77698dbb2f9d04846389bb
ZapBot ⚡ <70973410+ZapBot-dev@users.noreply.github.com>
2025-05-28 14:05:59 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,7 +4,7 @@ import time
 import requests
 
 app = Flask('')
-
+link = " https://telegram-bot-downloader-f0x2.onrender.com" # ex : https://telegram-bot-downloader-f0x2.onrender.com
 @app.route('/')
 def home():
     return "I’m alive!"
@@ -13,19 +13,17 @@ def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    # شغل السيرفر في ثريد منفصل
     t = Thread(target=run)
     t.start()
     
-    # استهداف السيرفر نفسه كل فترة عشان يفضل نشط
     def self_ping():
         while True:
             try:
                 print("[Pinger] Sending request to self...")
-                requests.get("https://telegram-bot-downloader-f0x2.onrender.com:8080/")
+                requests.get(f"{linlk}:8080/")
             except Exception as e:
                 print(f"[Pinger] Failed to ping self: {e}")
-            time.sleep(600)  # كل 10 دقائق
+            time.sleep(200)  
 
     ping_thread = Thread(target=self_ping)
     ping_thread.start()
